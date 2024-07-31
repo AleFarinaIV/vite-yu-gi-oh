4b98dfe34214d14f55723715283c9f988f043a1c
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-07-31 17:32:01 +0200

    [UPD] component Card compleated

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,16 +1,32 @@
 <script>
 export default {
 
+    props: {
+        card: Object,
+        image: Object
+    }
 }
+
 
 </script>
 
 <template>
-    
+
+    <img :src="image.image_url" alt="">
+    <h5>{{ card.name }}</h5>
+    <p>{{ card.race }}</p>
+
 </template>
 
 <style scoped>
 
-    
+img {
+    width: 200px;
+    object-fit: cover;
+}
+h5 {
+    margin-top: 10px;
+    color: white;
+}
 
 </style>
--- src/components/CardSection.vue
+++ src/components/CardSection.vue
@@ -1,5 +1,10 @@
 <script>
+import Card from "./Card.vue"
+
 export default {
+    components: {
+        Card,
+    },
     props: {
         card_section: Object
     },
@@ -10,9 +15,7 @@ export default {
 <template>
     
     <div id="cards_section" class="content p-0">
-        <img v-for="image, index in card_section.card_images" :key="`img-${index}`" :src="image.image_url" alt="">
-        <h5>{{ card_section.name }}</h5>
-        <p>{{ card_section.race }}</p>
+        <Card v-for="image, index in card_section.card_images" :key="`img-${index}`" :card="card_section" :image="image" />
     </div>
 
 </template>
@@ -25,14 +28,6 @@ export default {
         background-color: orange;
         width: 200px;
         margin-bottom: 20px;
-        img {
-            width: 200px;
-            object-fit: cover;
-        }
-        h5 {
-            margin-top: 10px;
-            color: white;
-        }
     }
 
 </style>
